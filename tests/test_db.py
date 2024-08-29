dba21b9fc38c9c4b33ac17edcfccddf21eb258e4
merweeeeen <melvinngchuanjie@gmail.com>
2024-08-29 10:40:42 +0800

    created tests cases (not completed)

---
+++ tests/test_db.py
@@ -0,0 +1,11 @@
+# Beanie uses Motor async client under the hood
+from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from app.conversation.schema import ConversationFull
+
+client = AsyncIOMotorClient("mongodb://localhost:27017")
+
+
+async def initiate_test_db():
+    await init_beanie(database=client.testdb, document_models=[ConversationFull])
