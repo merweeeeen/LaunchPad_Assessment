56d1f6105d6c088e540cb05fbdff94005c8fa08c
merweeeeen <melvinngchuanjie@gmail.com>
2024-08-31 00:00:28 +0800

    added cors middleware

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.common.db import initiate_db
 from app.conversations.main import router as conversation_router
@@ -14,6 +15,16 @@ async def init_db(app: FastAPI):
 
 
 app = FastAPI(title="LLM Interaction API", lifespan=init_db)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(conversation_router)
 app.include_router(queries_router)
