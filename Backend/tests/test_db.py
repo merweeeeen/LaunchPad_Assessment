7e2256e7ff8ea785e550ad516c028348acd44473
merweeeeen <melvinngchuanjie@gmail.com>
2024-08-30 23:14:23 +0800

    refactored backend and done testing (couldnt test the 500 errors)

--- Backend/tests/test_db.py
+++ Backend/tests/test_db.py
@@ -1,10 +1,10 @@
-# Beanie uses Motor async client under the hood
 from app.common.schema import ConversationFull
 from beanie import init_beanie
-from motor.motor_asyncio import AsyncIOMotorClient
-
-client = AsyncIOMotorClient("mongodb://host.docker.internal:27017")
+from mongomock_motor import AsyncMongoMockClient
 
 
 async def initiate_test_db():
-    await init_beanie(database=client.testdb, document_models=[ConversationFull])
+    client = AsyncMongoMockClient()
+    await init_beanie(
+        document_models=[ConversationFull], database=client.get_database(name="db")
+    )
